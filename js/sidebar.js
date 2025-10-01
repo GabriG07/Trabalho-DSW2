f30bf8fb9daa08ef48357e758898c152664d172f
gabriel <gabrihenriquer@gmail.com>
2025-10-01 11:56:30 -0300

    melhorado ainda mais a responsividade entre sidebar e conteudo principal

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,6 +2,8 @@
   const sidebar = document.querySelector('.sidebar');
   const main = document.querySelector('.main-content');
   const btn_padding = getComputedStyle(btn).paddingLeft;
+  const btn_left = getComputedStyle(btn).left;
+  const btn_top = getComputedStyle(btn).top;
 
   btn.addEventListener('click', () => {
     sidebar.classList.toggle('closed');
@@ -9,9 +11,14 @@
 
     if(getComputedStyle(btn).paddingLeft != "0px"){
         btn.style.paddingLeft="0px";
+        btn.style.left="10px";
+        btn.style.top="15px";
     }
     else{
       btn.style.paddingLeft=  btn_padding;
+      btn.style.left = btn_left;
+      btn.style.top = btn_top;
+      
     }
     
   });
